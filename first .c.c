b00c77893c2b8f510b04f03950edf1af414726b5
Jelaxsi <rajanjela22@gmail.com>
2025-02-19 09:56:38 +0530

    Added new file and updated second.c file

--- first .c.c
+++ first .c.c
@@ -4,7 +4,6 @@ int main(){
   printf("Welcome to the fundamental of programming session\n");
   printf("SEU/IS/22/ICT/068\n");
   printf("Jelaxsi Kularasan");
-  printf("19.02.2025");
 
   return 0;
 
